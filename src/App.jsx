ce0334420317ac06ff528997e7058652cc95d3b5
Youssef Emad <youssefweb120@gmail.com>
2023-08-05 19:30:58 +0300

    fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import GenreMovieList from "./Components/GenreMovieList";
 import Header from "./Components/Header";
+import ProductionHouse from "./Components/ProductionHouse";
 import Slider from "./Components/Slider";
+import './App.css'
 
 function App() {
   return (
     <div>
       <Header />
       <Slider />
+      <ProductionHouse />
+      <GenreMovieList />
     </div>
   );
 }
---
+++ src/Components/MovieList.jsx
@@ -0,0 +1,64 @@
+import { Fragment, useEffect, useRef, useState } from "react";
+import GlobalApi from "../Services/GlobalApi";
+import { IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5";
+import MovieCard from "./MovieCard";
+import HrMovieCard from "./HrMovieCard";
+
+// eslint-disable-next-line react/prop-types
+const MovieList = ({ genreId, index_ }) => {
+  const [movieList, setMovieList] = useState([]);
+  const elementRef = useRef(null);
+  useEffect(() => {
+    getMovieByGenreId();
+  }, []);
+
+  const getMovieByGenreId = () => {
+    GlobalApi.getMovieByGenreId(genreId).then((resp) => {
+      // console.log(resp.data.results)
+      setMovieList(resp.data.results);
+    });
+  };
+
+  const slideRight = (element) => {
+    element.scrollLeft += 500;
+  };
+  const slideLeft = (element) => {
+    element.scrollLeft -= 500;
+  };
+  return (
+    <div key={genreId} className=" relative">
+      <IoChevronBackOutline
+        onClick={() => slideLeft(elementRef.current)}
+        className={`text-[50px] text-white
+    p-2 z-10 cursor-pointer 
+     hidden md:block absolute
+     ${index_ % 3 == 0 ? "mt-[80px]" : "mt-[150px]"} `}
+      />
+      <div
+        key={genreId}
+        ref={elementRef}
+        className="flex overflow-x-auto gap-8
+        scrollbar-hide scroll-smooth pt-4 px-3 pb-4"
+      >
+        {movieList.map((item, index) => (
+          <Fragment key={index}>
+            {index_ % 3 == 0 ? (
+              <HrMovieCard movie={item} key={index} />
+            ) : (
+              <MovieCard movie={item} key={index} />
+            )}
+          </Fragment>
+        ))}
+      </div>
+      <IoChevronForwardOutline
+        onClick={() => slideRight(elementRef.current)}
+        className={`text-[50px] text-white hidden md:block
+           p-2 cursor-pointer z-10 top-0
+            absolute right-0 
+            ${index_ % 3 == 0 ? "mt-[80px]" : "mt-[150px]"}`}
+      />
+    </div>
+  );
+};
+
+export default MovieList;
