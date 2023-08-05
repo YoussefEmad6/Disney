ececcb950e0044af342f198e9645cc340a1adda8
Youssef Emad <youssefweb120@gmail.com>
2023-08-05 03:55:50 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Components/Header";
+import Slider from "./Components/Slider";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Slider />
+    </div>
+  );
+}
+export default App;
---
+++ src/Components/Slider.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Slider = () => {
+  return (
+    <div className=''>Slider</div>
+  )
+}
+
+export default Slider
